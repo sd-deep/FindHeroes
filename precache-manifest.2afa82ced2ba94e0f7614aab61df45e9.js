08ee6c813ca8ca79d05d56e6174c02290d4a7d3d
sd-deep <shankhadeepdas8@gmail.com>
2020-05-13 03:12:11 +0530

    Updates

--- precache-manifest.2afa82ced2ba94e0f7614aab61df45e9.js
+++ precache-manifest.2afa82ced2ba94e0f7614aab61df45e9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8bce213a92545c1311d1fc9a0dcdfeab",
+    "revision": "8322ff109c51697aae39f805453a98d5",
     "url": "/FindHeroes/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/FindHeroes/static/css/2.74303661.chunk.css"
   },
   {
-    "revision": "83f985e120fc2cf4444a",
+    "revision": "bdca614eee795539afb1",
     "url": "/FindHeroes/static/css/main.74596f2c.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/FindHeroes/static/js/2.054ab9b9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "83f985e120fc2cf4444a",
-    "url": "/FindHeroes/static/js/main.ec8b4ff8.chunk.js"
+    "revision": "bdca614eee795539afb1",
+    "url": "/FindHeroes/static/js/main.53f28045.chunk.js"
   },
   {
     "revision": "41d9f543eaa0acf99345",
