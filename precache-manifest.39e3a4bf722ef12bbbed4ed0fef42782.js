3636918f31b75c5b223253e92f541656c7c844c7
sd-deep <shankhadeepdas8@gmail.com>
2020-05-13 03:01:34 +0530

    Updates

---
+++ precache-manifest.39e3a4bf722ef12bbbed4ed0fef42782.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bce213a92545c1311d1fc9a0dcdfeab",
+    "url": "/FindHeroes/index.html"
+  },
+  {
+    "revision": "a24520bac415c1740e2b",
+    "url": "/FindHeroes/static/css/2.74303661.chunk.css"
+  },
+  {
+    "revision": "83f985e120fc2cf4444a",
+    "url": "/FindHeroes/static/css/main.74596f2c.chunk.css"
+  },
+  {
+    "revision": "a24520bac415c1740e2b",
+    "url": "/FindHeroes/static/js/2.054ab9b9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/FindHeroes/static/js/2.054ab9b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f985e120fc2cf4444a",
+    "url": "/FindHeroes/static/js/main.ec8b4ff8.chunk.js"
+  },
+  {
+    "revision": "41d9f543eaa0acf99345",
+    "url": "/FindHeroes/static/js/runtime-main.882284d6.js"
+  }
+]);
